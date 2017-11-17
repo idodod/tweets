fe238ae1dd01bf0b34d757bf1516300dedf26459
Ido David <idodod@gmail.com>
2017-11-16 23:24:18 -0500

    removing unecessary actions and views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_action :set_tweet, only: [:update]
 
   # GET /tweets
   # GET /tweets.json
@@ -7,35 +7,6 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
-  # GET /tweets/1
-  # GET /tweets/1.json
-  def show
-  end
-
-  # GET /tweets/new
-  def new
-    @tweet = Tweet.new
-  end
-
-  # GET /tweets/1/edit
-  def edit
-  end
-
-  # POST /tweets
-  # POST /tweets.json
-  def create
-    @tweet = Tweet.new(tweet_params)
-
-    respond_to do |format|
-      if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
-        format.json { render :show, status: :created, location: @tweet }
-      else
-        format.html { render :new }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # PATCH/PUT /tweets/1
   # PATCH/PUT /tweets/1.json
@@ -48,16 +19,6 @@ class TweetsController < ApplicationController
         format.html { render :edit }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /tweets/1
-  # DELETE /tweets/1.json
-  def destroy
-    @tweet.destroy
-    respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
